21ef28dc509ec565db9ab3692bb87883473c2b20
mddanish867 <mddanish867@gmail.com>
2024-02-29 21:43:04 +0530

    removed vercel speed insight from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import './index.css';
 import App from './Container/App';
 import { Provider } from 'react-redux';
 import store from './Storage/Redux/store';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(
@@ -12,7 +11,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={store}>
 <App /> 
-<SpeedInsights/>
   </Provider>
       
 );
